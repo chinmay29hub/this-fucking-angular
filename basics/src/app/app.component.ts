04002395d31315b1c4fa61d9d0abfb5552b35d1e
Chinmay Sonawane <chinmaysonawane@bdo.in>
2024-03-19 20:31:31 +0530

    02 | 17

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `<h1>Hello World</h1>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {

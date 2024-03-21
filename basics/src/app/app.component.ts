a3dd7ca450d5c5b8e0a4ad2d5330abec994e527e
Chinmay Sonawane <chinmaysonawane@bdo.in>
2024-03-21 20:36:15 +0530

    04 | 04

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -2,10 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: `<h1>Hello World</h1>`,
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html',
+  // template: `<h1>Hello World</h1>`,
+  // styleUrl: './app.component.css',
+  styleUrls : ["./app.component.css"],
+  // styles : [""]
 })
 export class AppComponent {
-  title = 'basics';
+  name = "chinmay"
+  imageUrl = "https://picsum.photos/id/237/500/500"
+
+  getName () {
+    return this.name
+  }
+
+  changeImage (e : KeyboardEvent) {
+    this.imageUrl = (e.target as HTMLInputElement).value
+  }
+
 }

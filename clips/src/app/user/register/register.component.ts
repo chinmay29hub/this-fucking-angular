4c20666051bd6254d58d1f0ad20d00a3a506d2f0
Chinmay Sonawane <chinmaysonawane@bdo.in>
2024-03-28 16:19:10 +0530

    07 | 19

--- clips/src/app/user/register/register.component.ts
+++ clips/src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  registerForm = new FormGroup({
+    name : new FormControl("", [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    email : new FormControl("", [
+      Validators.required,
+      Validators.email
+    ]),
+    age : new FormControl("", [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(120)
+    ]),
+    password : new FormControl("", [
+      Validators.required,
+      Validators.pattern(/^(?=.*\d)(?=.*[a−z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+    ]),
+    confirm_password : new FormControl("", [
+      Validators.required
+    ]),
+    phoneNumber : new FormControl("", [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10)
+    ])
+  })
+
+  register () {
+    console.log("Form submitted")
+  }
 
 }

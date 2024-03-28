8d5dfd81ef0b32cbca6462bed4db2661e9ff30d4
Chinmay Sonawane <chinmaysonawane@bdo.in>
2024-03-28 16:19:10 +0530

    07 | 19

--- clips/src/app/shared/shared.module.ts
+++ clips/src/app/shared/shared.module.ts
@@ -3,25 +3,35 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
 // import { ModalService } from '../services/modal.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideEnvironmentNgxMask, NgxMaskDirective } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
 
 
 @NgModule({
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
+    AlertComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgxMaskDirective
   ],
   exports: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
+    AlertComponent
   ],
-  // providers: [
-  //   ModalService
-  // ]
+  providers : [
+    provideEnvironmentNgxMask()
+  ]
 })
 export class SharedModule { }

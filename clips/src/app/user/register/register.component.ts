f41a43edc8204c79b20926cbb81f8b4ef2b2890b
Chinmay Sonawane <chinmaysonawane@bdo.in>
2024-03-31 22:28:58 +0530

    12 | 02

--- clips/src/app/user/register/register.component.ts
+++ clips/src/app/user/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import IUser from '../../models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +11,7 @@ import IUser from '../../models/user.model';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  constructor(private auth : AuthService) {
+  constructor(private auth : AuthService, private emailTaken : EmailTaken) {
 
   }
 
@@ -21,7 +23,7 @@ export class RegisterComponent {
 
   registerForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.email], [this.emailTaken.validate]),
     age: new FormControl<number | null>(null, [
       Validators.required,
       Validators.min(18),
@@ -39,7 +41,7 @@ export class RegisterComponent {
       Validators.minLength(10),
       Validators.maxLength(10),
     ]),
-  });
+  }, [ RegisterValidators.match("password", "confirm_password") ]);
 
   async register() {
     // console.log("Form submitted")
@@ -51,7 +53,7 @@ export class RegisterComponent {
     try {
       await this.auth.createUser(this.registerForm.value as IUser)
     } catch (e: any) {
-      console.error(e);
+      // console.error(e);
 
       switch (e.code) {
         case 'auth/email-already-in-use':

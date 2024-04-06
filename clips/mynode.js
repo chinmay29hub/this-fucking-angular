0b899fdbd598e59a048507f7e7577bd3f6693881
Chinmay Sonawane <chinmaysonawane@bdo.in>
2024-04-07 00:01:50 +0530

    clips hide secrets final deployment

---
+++ clips/mynode.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+const dotenv = require('dotenv').config({path: 'src/.env'});
+
+// Content for environment.ts file
+const envFileContent = `export const environment = {
+  production: '${process.env.PRODUCTION}',
+  firebase: {
+    apiKey: '${process.env.API_KEY}',
+    authDomain: '${process.env.AUTH_DOMAIN}',
+    projectId: '${process.env.PROJECT_ID}',
+    storageBucket: '${process.env.STORAGE_BUCKET}',
+    appId: '${process.env.APP_ID}',
+  },
+};
+`;
+
+// Path to the target environment.ts file
+const targetFolderPath = path.join(__dirname, './src/environments');
+const targetFilePath = path.join(targetFolderPath, 'environment.ts');
+
+// Function to create environments folder and environment.ts file
+const createEnvironmentFiles = () => {
+    // Check if environments folder exists, if not, create it
+    if (!fs.existsSync(targetFolderPath)) {
+        fs.mkdirSync(targetFolderPath);
+    }
+
+    // Write environment.ts file
+    fs.writeFile(targetFilePath, envFileContent, (err) => {
+        if (err) {
+            console.error(err);
+            throw err;
+        } else {
+            console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+        }
+    });
+};
+
+// Invoke function to create environment files
+createEnvironmentFiles();

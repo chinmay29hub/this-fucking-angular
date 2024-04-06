43a26c10a89be8e9d50efd5ae801cd8fae52d643
Chinmay Sonawane <chinmaysonawane@bdo.in>
2024-04-06 23:52:27 +0530

    weather app deployment fixed

--- weatherApp/mynode.js
+++ weatherApp/mynode.js
@@ -2,20 +2,37 @@ const fs = require('fs');
 const path = require('path');
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
-const dotenv = require('dotenv').config({path: 'src/.env'}); ;
+const dotenv = require('dotenv').config({path: 'src/.env'});
 
-const envFile = `export const environment = {
+// Content for environment.ts file
+const envFileContent = `export const environment = {
     production: '${process.env.PRODUCTION}',
     weatherApiBaseUrl: '${process.env.WEATHERBASEAPIURL}',
     appid : '${process.env.APPID}'
 };
 `;
-const targetPath = path.join(__dirname, './src/environments/environment.ts');
-fs.writeFile(targetPath, envFile, (err) => {
-    if (err) {
-        console.error(err);
-        throw err;
-    } else {
-        console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+
+// Path to the target environment.ts file
+const targetFolderPath = path.join(__dirname, './src/environments');
+const targetFilePath = path.join(targetFolderPath, 'environment.ts');
+
+// Function to create environments folder and environment.ts file
+const createEnvironmentFiles = () => {
+    // Check if environments folder exists, if not, create it
+    if (!fs.existsSync(targetFolderPath)) {
+        fs.mkdirSync(targetFolderPath);
     }
-});
+
+    // Write environment.ts file
+    fs.writeFile(targetFilePath, envFileContent, (err) => {
+        if (err) {
+            console.error(err);
+            throw err;
+        } else {
+            console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+        }
+    });
+};
+
+// Invoke function to create environment files
+createEnvironmentFiles();
